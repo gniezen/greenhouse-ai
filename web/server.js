ed7ef648602c524e491ee4fd543d8d9b52d951b5
Gerrit Niezen <info@embeddedloop.com>
2022-07-22 15:51:37 +0100

    change port to 80

--- web/server.js
+++ web/server.js
@@ -11,7 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // this is where we'll handle our various routes from
 const routes = require('./routes/routes.js')(app, fs);
 
-// finally, launch our server on port 3001.
-const server = app.listen(3001, () => {
+// finally, launch our server on port 80.
+// change this to 3001 if running locally
+const server = app.listen(80, () => {
   console.log('listening on port %s...', server.address().port);
 });
